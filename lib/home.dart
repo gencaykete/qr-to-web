706398dacd305aa2498e69e9b917fd4eec0d23cb
gencaykete <gencaykt@gmail.com>
2021-07-14 16:01:37 +0300

    IOS Icon

--- lib/home.dart
+++ lib/home.dart
@@ -15,7 +15,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   InterstitialAd? _interstitialAd;
 
   bool _isInterstitialAdReady = false;
@@ -29,12 +28,8 @@ class _HomePageState extends State<HomePage> {
           this._interstitialAd = ad;
           ad.fullScreenContentCallback = FullScreenContentCallback(
             onAdDismissedFullScreenContent: (ad) {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ScannerPage()
-                  )
-              );
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ScannerPage()));
             },
           );
 
@@ -61,26 +56,28 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset('images/android.png'),
+        child: GestureDetector(
+            onTap: () {
+              if (Helper.isScanned) {
+                _interstitialAd?.show();
+              } else {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ScannerPage()));
+              }
+            },
+            child: Image.asset('images/android.png')),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          if(Helper.isScanned){
+          if (Helper.isScanned) {
             _interstitialAd?.show();
-            /*if(_isInterstitialAdReady){
-              _interstitialAd?.show();
-            }else {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => ScannerPage()));
-            }*/
-          }else {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => ScannerPage()));
+          } else {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => ScannerPage()));
           }
         },
         child: Icon(Icons.qr_code),
       ),
     );
   }
-
 }
